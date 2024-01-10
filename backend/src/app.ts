f0c06c4f1058b2228d09ebb5835c888da312c206
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-10 18:10:40 -0300

    <Update> Error handling system

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,24 @@
-import { Express } from "express";
-import express from 'express'
+import "express-async-errors";
+import {AppError} from "./error/AppError"
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) =>{
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: "error",
+            message: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "error",
+        message: `Error Interno no servidor - ${err.message}`
+    })
+})
 
 
 
