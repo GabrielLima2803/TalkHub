2a28540cfb3886be49c35cee5052d7c6f5ac9664
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-10 23:13:57 -0300

    <Update> Add routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import "express-async-errors";
 import {AppError} from "./error/AppError"
 import express, { NextFunction, Request, Response } from 'express';
+import { routes } from "./routes";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(routes)
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) =>{
     if (err instanceof AppError) {
