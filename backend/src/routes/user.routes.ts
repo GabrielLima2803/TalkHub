7419d560fe0d585690c38c55d7987fa4cc9efe7e
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-11 00:55:40 -0300

    <Update> Add router get-all

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { UserController } from "../controller/UserController";
+import checkToken from "../middleware/checkToken";
 
 
 const createUser = new UserController.CreateUserController();
+const loginUser = new UserController.AuthUserController();
+const getAllUsers = new UserController.GetAllUsersController();
 
 const userRoutes = Router();
 
 userRoutes.post("/register", createUser.handle);
+userRoutes.post("/login", loginUser.handle);
+userRoutes.get("/get-all", checkToken, getAllUsers.handle);
 
 export {userRoutes}
