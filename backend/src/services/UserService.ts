879ea3e40d8e391b9801434c2304d419f1d0d57d
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-10 23:15:29 -0300

    <Create> Namespace UserService

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -2,27 +2,43 @@ import { User } from "@prisma/client";
 import { UserDTO } from "../dtos/UserDTO";
 import { AppError } from "../error/AppError";
 import { prisma } from "../prisma/client";
+import argon2 from 'argon2';
+import { isValidEmail } from "../middleware/isValidEmail";
 
-export class CreateUserService {
-    async execute({username, email, password}: UserDTO): Promise<User> {  
-        const userExist = await prisma.user.findUnique({
+export namespace UserService {
+    
+    export class CreateUserService {
+      async execute({ username, email, password }: UserDTO): Promise<User> {
+        try {
+          const userExist = await prisma.user.findUnique({
             where: {
-                email: email
+              email: email
             }
-        });
+          });
+          
+          if (!isValidEmail(email)) {
+            throw new AppError("E-mail inválido");
+          }
 
-        if (userExist) {
-            throw new AppError("User já existe");
+          if (userExist) {
+            throw new AppError("Usuário já existe");
+          }
+  
+          const hashedPassword = await argon2.hash(password);
+  
+          const user = await prisma.user.create({
+            data: {
+              username,
+              email,
+              password: hashedPassword
+            }
+          });
+  
+          return user;
+        } catch (error) {
+          throw error; 
         }
-
-        const user = prisma.user.create({
-            data:{
-                username,
-                email,
-                password
-            }
-        });
-
-        return user;
+      }
     }
-}
+  }
+  
