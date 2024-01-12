a1efbf77dae214132015475cead63a356dfa266e
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-12 18:11:28 -0300

    <Update> Add routes forget-password and reset-paasword

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -6,11 +6,15 @@ import checkToken from "../middleware/checkToken";
 const createUser = new UserController.CreateUserController();
 const loginUser = new UserController.AuthUserController();
 const getAllUsers = new UserController.GetAllUsersController();
+const forgetPassword = new UserController.ForgetPasswordController();
+const resetPassword = new UserController.ResetPasswordController();
 
 const userRoutes = Router();
 
 userRoutes.post("/register", createUser.handle);
 userRoutes.post("/login", loginUser.handle);
 userRoutes.get("/get-all", checkToken, getAllUsers.handle);
+userRoutes.post("/forget-password", forgetPassword.handle);
+userRoutes.post("/reset-password", resetPassword.handle)
 
 export {userRoutes}
