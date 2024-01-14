5d1bccc111e9a1ed020823ad36145a92a5a84484
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-13 22:13:50 -0300

    <Update> Add Routers

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+import ForgetPasswordView from '@/views/auth/ForgetPasswordView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/forget-password',
+      name: 'forget',
+      component: ForgetPasswordView
     }
   ]
 })
