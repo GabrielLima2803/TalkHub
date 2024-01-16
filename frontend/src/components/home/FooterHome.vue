eec6786672b605ee1c16982da02ca82aaa98a4cd
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-15 22:57:20 -0300

    <Create> FooterHome Component

---
+++ frontend/src/components/home/FooterHome.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="footer">
+        <h1 class="h1-footer">Feito por Gabriel Lima De Souza</h1>
+        <a href="https://github.com/GabrielLima2803/TalkHub" target="_blank" class="reset">
+        <h3>Código no Github</h3>
+        </a>
+    </div>
+</template>
+  
+<style>
+.footer {
+    background-color: rgb(34, 34, 34);
+    color: white;
+    padding-top: 40px;
+    margin: 0;
+    text-align: center;
+    margin-top: auto;
+}
+
+.h1-footer {
+    font-size: 16px;
+    margin-bottom: 10px;
+}
+
+h3 {
+    font-size: 14px;
+    margin-top: 0;
+    padding-bottom: 13px;
+    cursor: pointer;
+    font-style: italic;
+    margin-bottom: 0px;
+}
+.reset{
+    text-decoration: none;
+    color: white;
+}
+</style>
+  
