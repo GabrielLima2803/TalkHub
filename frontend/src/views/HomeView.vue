76ba24f52ac7ed4d73c4c7446318a9e75246bff3
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-15 22:58:09 -0300

    <Update> New Page Home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,57 +1,21 @@
+<script setup>
+import HomeComp from '@/components/home/HomeComp.vue';
+import FooterHome from '@/components/home/FooterHome.vue'
+import HeaderHome from '@/components/home/HeaderHome.vue'
+
+</script>
+
 <template>
-  <div>
-    <h1>Chat</h1>
-    <div v-for="message in messages" :key="message.id">
-      {{ message.sender }}: {{ message.message }}
-    </div>
-    <input v-model="usernameInput" placeholder="Digite seu username" />
-    <button @click="joinChat">Entrar no Chat</button>
-    <input v-model="messageInput" @keyup.enter="sendMessage" :disabled="!joined" placeholder="Digite sua mensagem" />
-  </div>
+<div class="bg">
+<header-home/>
+<home-comp/>
+<footer-home/>
+</div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue';
-import io from 'socket.io-client';
+<style>
+.bg{
+  background-color: rgb(34, 34, 34);
 
-const messages = ref([]);
-const usernameInput = ref('');
-const messageInput = ref('');
-const socket = io('http://localhost:3000', {
-  transports: ['websocket'], 
-  upgrade: false,
-});
-
-let joined = false;
-
-const joinChat = () => {
-  if (usernameInput.value.trim() !== '') {
-    socket.emit('join', { username: usernameInput.value });
-    joined = true;
-  }
-};
-
-onMounted(() => {
-  socket.on('userConnected', (userData) => {
-    messages.value.push({ sender: 'Chat Bot', message: `${userData.username} entrou no chat.` });
-  });
-
-  socket.on('onlineUsers', (onlineUsers) => {
-    console.log(`Usuários online: ${onlineUsers.join(', ')}`);
-  });
-
-  socket.on('receiveMessage', (data) => {
-    messages.value.push(data);
-  });
-
-  socket.on('userDisconnected', (userData) => {
-    messages.value.push({ sender: 'Chat Bot', message: `${userData.username} saiu do chat.` });
-  });
-});
-
-const sendMessage = () => {
-  socket.emit('sendMessage', { sender: usernameInput.value, receiverSocketId: 'Lima', message: messageInput.value });
-  messages.value.push({ sender: usernameInput.value, message: messageInput.value });
-  messageInput.value = '';
-};  
-</script>
+}
+</style>
