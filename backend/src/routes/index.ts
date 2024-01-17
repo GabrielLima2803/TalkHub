ee02b18bc104c26dbda453542c5403218baa94aa
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-17 01:42:38 -0300

    <Update> Add Chat Routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
+import { chatRouter } from "./chat.routes";
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
+routes.use("/conversation", chatRouter);
 
 export {routes}
