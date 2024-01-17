d3cbadf38fdfc8490390664a4829a70d66ed4883
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-17 01:42:48 -0300

    <Create> Chat Services

---
+++ backend/src/services/ChatService.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from '@prisma/client';
+import { AppError } from '../error/AppError';
+
+const prisma = new PrismaClient();
+
+export namespace chatService {
+  export class CreateConversationService {
+    async execute({ userId1, userId2 }: { userId1: number; userId2: number }) {
+      try {
+        const user1 = await prisma.user.findUnique({ where: { id: userId1 } });
+        const user2 = await prisma.user.findUnique({ where: { id: userId2 } });
+
+        if (!user1 || !user2) {
+          throw new AppError('Usuários não encontrados');
+        }
+
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              connect: [{ id: userId1 }, { id: userId2 }],
+            },
+          },
+        });
+
+        return conversation;
+      } catch (error) {
+        console.error('Erro ao criar a conversa:', error);
+        throw error;
+      }
+    }
+  }
+  export class GetConversationInfoService {
+    async execute(conversationId: number) {
+      try {
+        const conversation = await prisma.conversation.findUnique({
+          where: {
+            id: conversationId,
+          },
+          include: {
+            participants: {
+              select: {
+                id: true,
+                username: true,
+                email: true,
+              },
+            },
+            messages: {
+              select: {
+                id: true,
+                content: true,
+                createdAt: true,
+                sender: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+              orderBy: {
+                createdAt: 'asc',
+              },
+            },
+          },
+        });
+  
+        return conversation;
+      } catch (error) {
+        console.error('Error getting conversation info:', error);
+        throw error;
+      }
+    }
+  }
+}
