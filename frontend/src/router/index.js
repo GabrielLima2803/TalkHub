0c015dd05a15bf8959333babcdc089690d31585a
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-16 22:50:08 -0300

    <Update> Add Router chat

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,8 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import ForgetPasswordView from '@/views/auth/ForgetPasswordView.vue'
-import ResetPasswordComp from '@/components/auth/ResetPasswordComp.vue'
+import ResetPasswordView from '@/views/auth/ResetPasswordView.vue'
+import ChatView from '@/views/chat/ChatView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,7 +32,12 @@ const router = createRouter({
     {
       path: '/reset-password',
       name: 'reset',
-      component: ResetPasswordComp
+      component: ResetPasswordView
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatView
     }
   ]
 })
