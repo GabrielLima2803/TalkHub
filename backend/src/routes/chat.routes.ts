832175960d94a214c32a25c483c4205b8e2a5f62
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-17 21:30:11 -0300

    <Update> Add router create-message

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -3,11 +3,13 @@ import { ChatController } from "../controller/ChatController";
 import checkToken from "../middleware/checkToken";
 
 const createConversation = new ChatController.CreateConversationController();
-const getInfoConversation = new ChatController.GetConversationInfoController()
+const getInfoConversation = new ChatController.GetConversationInfoController();
+const createMessage = new ChatController.CreateMessageController();
 
 const chatRouter = Router();
 
 chatRouter.post('/create/:userId1/:userId2', checkToken, createConversation.handle);
 chatRouter.get('/get-info/:conversationId', checkToken, getInfoConversation.handle);
+chatRouter.post('/create-message', checkToken, createMessage.handle)
 
 export {chatRouter}
