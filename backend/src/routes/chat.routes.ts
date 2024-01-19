863661268fa12c7438989ca6068805e7ab344c25
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-18 21:34:22 -0300

    <Update> Remove checkToken

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -8,8 +8,8 @@ const createMessage = new ChatController.CreateMessageController();
 
 const chatRouter = Router();
 
-chatRouter.post('/create/:userId1/:userId2', checkToken, createConversation.handle);
-chatRouter.get('/get-info/:conversationId', checkToken, getInfoConversation.handle);
-chatRouter.post('/create-message', checkToken, createMessage.handle)
+chatRouter.post('/create/:userId1/:userId2',  createConversation.handle);
+chatRouter.get('/get-info/:conversationId',  getInfoConversation.handle);
+chatRouter.post('/create-message',  createMessage.handle)
 
 export {chatRouter}
