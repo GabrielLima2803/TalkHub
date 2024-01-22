205f30a1728ac9b2b8c3a4c6a6c0ca0aa2dda4ac
GabrielLima2803 <gabriellima2803@gmail.com>
2024-01-22 00:10:27 -0300

    <Create> Html Header Chat

---
+++ frontend/src/components/chat/HeaderChat.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header-chat">
+        <div>
+            Logout
+        </div>
+        <div>
+            Chat de 
+        </div>
+        <div>
+            home
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
