3271a7b391fb3edce6099c0ed06aa7e543782012
GabrielLima2803 <gabriellima2803@gmail.com>
2024-12-04 05:46:31 -0300

    fix: Conexão com o mongo

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 
 async function conectarAoMongoDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
+        await mongoose.connect('mongodb+srv://Talkhub:admin@cluster0.d01w1.mongodb.net/Talkhub?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
         console.log('Conectado ao MongoDB');
     } catch (erro) {
         console.error('Erro ao conectar ao MongoDB:', erro);
     }
 }
-
 module.exports = {
     conectarAoMongoDB,
-};
+}
